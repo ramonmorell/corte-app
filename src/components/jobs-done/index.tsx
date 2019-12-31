62b0d83834786ed61d9d2538b2e14fb886f3d647
ramon <ramonmorell@yahoo.es>
2019-12-31 10:14:35 +0100

    chore(corte-app): clean unused imports

--- src/components/jobs-done/index.tsx
+++ src/components/jobs-done/index.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useMemo} from 'react';
+import React, {useCallback, useMemo} from 'react';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
